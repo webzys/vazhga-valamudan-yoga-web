da0b10cb046f5b2d69bc98db824c1dc5527b2aab
aravindan-kareinfinity <130987577+aravindan-kareinfinity@users.noreply.github.com>
2025-04-06 13:03:19 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/vazhga-valamudan-yoga-web/',
   plugins: [
     react(),
     mode === 'development' &&
